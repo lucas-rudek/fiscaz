15bab1bc227aa4481cb7013f3928d1648fb663a2
Lucas Rudek <lucas-rudek@hotmail.com>
2021-06-10 15:17:56 -0300

    api working!

--- src/components/container/Main.jsx
+++ src/components/container/Main.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import SubMenu from "./SubMenu.jsx";
+import api from "../../services/api.js";
 import { MainWrapper, MainTitle, MainInfoWrapper } from "../styles.js";
+
+api
+  .get("usuarios")
+  .then((res) => console.log(res.data))
+  .catch((err) => {
+    console.error(err);
+  });
 
 export default function Main() {
   return (
@@ -9,13 +17,15 @@ export default function Main() {
       <MainTitle>Abertura do Processo de Licitação</MainTitle>
       <MainInfoWrapper>
         <form>
-          <label for="fname">First name:</label>
+          <label htmlFor="name">First name:</label>
           <br />
-          <input type="text" id="fname" name="fname" />
+          <input type="text" id="name" name="name" />
           <br />
-          <label for="lname">Last name:</label>
+          <label htmlFor="surname">Last name:</label>
           <br />
-          <input type="text" id="lname" name="lname" />
+          <input type="text" id="surname" name="surname" />
+          <br />
+          <button type="submit">Enviar</button>
         </form>
       </MainInfoWrapper>
     </MainWrapper>
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://60f2p.sse.codesandbox.io"
+});
+
+export default api;
